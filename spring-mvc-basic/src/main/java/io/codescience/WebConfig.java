9bf82e65b71a105b94608d7ff5e2bdfb4a501b14
AlanLuo <iteamalan@hotmail.com>
2025-04-28 22:08:28 +0800

    添加购物车样式

--- spring-mvc-basic/src/main/java/io/codescience/WebConfig.java
+++ spring-mvc-basic/src/main/java/io/codescience/WebConfig.java
@@ -3,6 +3,7 @@ package io.codescience;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
     }
 }
